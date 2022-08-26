8b9b81b01cd9f3538c53c83a97abfe9e373d6286
Ivens <ivensarroxellas@hotmail.com>
2022-08-26 20:30:02 -0300

    ajuste

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,11 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER_ACTION } from '../actions/actionTypes';
+
 const INITIAL_STATE = {
   email: '', // string que armazena o email da pessoa usuária
 };
 
 const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case 'USER_ACTION':
+  case USER_ACTION:
     return { ...state,
       email: action.payload1,
     };
