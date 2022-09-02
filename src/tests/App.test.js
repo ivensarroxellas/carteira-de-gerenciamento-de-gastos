b00ffc04914653fd4c4e8ff0c57d84433980d62b
Ivens <ivensarroxellas@hotmail.com>
2022-09-02 14:41:29 -0300

    req 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,6 +5,7 @@ import renderWithRouter from './helpers/renderWith';
 import Login from '../pages/Login';
 import Wallet from '../pages/Wallet';
 import App from '../App';
+import WalletForm from '../components/WalletForm';
 
 describe('Verifica tela de login', () => {
   test('Verifica página de login', () => {
@@ -38,5 +39,18 @@ describe('Verifica tela da carteira', () => {
     renderWithRouter(<Wallet />);
     const walletlement = screen.getByRole('heading', { level: 2, name: /walletform/i });
     expect(walletlement).toBeInTheDocument();
+  });
+});
+// Teste abaixo tive auxílio do Rafael Miranda
+describe('Verifica se chama botão despesa', () => {
+  test('Verifica se despesa é adicionada', async () => {
+    renderWithRouter(<WalletForm />);
+    const inputValue = screen.getByTestId('value-input');
+    const inputDescription = screen.getByTestId('description-input');
+    const buttonAddDespesa = screen.getByRole('button', { name: /Adicionar/i });
+
+    userEvent.type(inputValue, '4000');
+    userEvent.type(inputDescription, 'Algo');
+    userEvent.click(buttonAddDespesa);
   });
 });
